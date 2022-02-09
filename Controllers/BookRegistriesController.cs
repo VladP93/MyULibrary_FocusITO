fe168ca7e91100feb024ceddcf1fb9d0697a8c8d
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2022-02-09 01:54:15 -0600

    Basic endpoints created

---
+++ Controllers/BookRegistriesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyULibrary.Models;
+
+namespace MyULibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookRegistriesController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public BookRegistriesController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BookRegistries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookRegistry>>> GetBookRegistry()
+        {
+            return await _context.BookRegistry.ToListAsync();
+        }
+
+        // GET: api/BookRegistries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookRegistry>> GetBookRegistry(int id)
+        {
+            var bookRegistry = await _context.BookRegistry.FindAsync(id);
+
+            if (bookRegistry == null)
+            {
+                return NotFound();
+            }
+
+            return bookRegistry;
+        }
+
+        // PUT: api/BookRegistries/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBookRegistry(int id, BookRegistry bookRegistry)
+        {
+            if (id != bookRegistry.IdBookRegistry)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bookRegistry).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookRegistryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BookRegistries
+        [HttpPost]
+        public async Task<ActionResult<BookRegistry>> PostBookRegistry(BookRegistry bookRegistry)
+        {
+            _context.BookRegistry.Add(bookRegistry);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBookRegistry", new { id = bookRegistry.IdBookRegistry }, bookRegistry);
+        }
+
+        // DELETE: api/BookRegistries/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BookRegistry>> DeleteBookRegistry(int id)
+        {
+            var bookRegistry = await _context.BookRegistry.FindAsync(id);
+            if (bookRegistry == null)
+            {
+                return NotFound();
+            }
+
+            _context.BookRegistry.Remove(bookRegistry);
+            await _context.SaveChangesAsync();
+
+            return bookRegistry;
+        }
+
+        private bool BookRegistryExists(int id)
+        {
+            return _context.BookRegistry.Any(e => e.IdBookRegistry == id);
+        }
+    }
+}
---
+++ ViewModels/PersonViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyULibrary.ViewModels
+{
+    public class PersonViewModel
+    {
+        [Key]
+        public int IdPerson { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public int IdRol { get; set; }
+    }
+}
