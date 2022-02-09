1441bc7b42a0fa3ded6cd38c56be0162906ce6ad
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2022-02-09 00:25:50 -0600

    Model and migration created

---
+++ Models/BookRegistry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyULibrary.Models
+{
+    public class BookRegistry
+    {
+        [Key]
+        public int IdBookRegistry { get; set; }
+        [Required]
+        public DateTime DateCheckout { get; set; }
+        [Required]
+        public DateTime DateReturn { get; set; }
+        [Required]
+        public bool Returned { get; set; }
+        public int IdBook { get; set; }
+        public Book Book { get; set; }
+        public int IdStudent { get; set; }
+        public Person Student { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyULibrary.Models;
 
 namespace MyULibrary
 {
@@ -26,6 +28,9 @@ namespace MyULibrary
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddDbContext<MyDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
